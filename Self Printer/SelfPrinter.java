f7cc5fe443d54d71a889724d96afdd1fe2f0a4b1
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-31 18:41:49 -0700

    Output is identical to program

--- Self Printer/SelfPrinter.java
+++ Self Printer/SelfPrinter.java
@@ -2,11 +2,11 @@ import java.util.LinkedList;
 
 public class SelfPrinter
 {
-	public static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s)
+	public static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s, String list)
 	{
 		lines.add(s);
 		s = s.replace("\\", "\\\\").replace("\n", "\\n").replace("\"","\\\"").replace("\t", "\\t");
-		String temp = "\t\tpapinv(lines,invocs,\"" + s + "\");";
+		String temp = "\t\tpapinv(" + list + ",invocs,\"" + s + "\",\"" + list + "\");";
 		invoc.add(temp);
 	}
 
@@ -15,25 +15,25 @@ public class SelfPrinter
 		LinkedList<String> lines = new LinkedList<String>();
 		LinkedList<String> invocs = new LinkedList<String>();
 		LinkedList<String> post = new LinkedList<String>();
-		papinv(lines,invocs,"import java.util.LinkedList;\n");
-		papinv(lines,invocs,"public class SelfPrinter\n{");
-		papinv(lines,invocs,"\tpublic static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s)\n\t{");
-		papinv(lines,invocs,"\t\tlines.add(s);");
-		papinv(lines,invocs,"\t\ts = s.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\n\", \"\\\\n\").replace(\"\\\"\",\"\\\\\\\"\").replace(\"\\t\", \"\\\\t\");");
-		papinv(lines,invocs,"\t\tString temp = \"\\t\\tpapinv(lines,invocs,\\\"\" + s + \"\\\");\";");
-		papinv(lines,invocs,"\t\tinvoc.add(temp);");
-		papinv(lines,invocs,"\t}\n");
-		papinv(lines,invocs,"\tpublic static void main(String[] args)\n\t{");
-		papinv(lines,invocs,"\t\tLinkedList<String> lines = new LinkedList<String>();");
-		papinv(lines,invocs,"\t\tLinkedList<String> invocs = new LinkedList<String>();");
-		papinv(lines,invocs,"\t\tLinkedList<String> post = new LinkedList<String>();");
-		papinv(post,invocs,"\t\tfor(String s : lines)");
-		papinv(post,invocs,"\t\t\tSystem.out.println(s);");
-		papinv(post,invocs,"\t\tfor(String s : invocs)");
-		papinv(post,invocs,"\t\t\tSystem.out.println(s);");
-		papinv(post,invocs,"\t\tfor(String s : post)");
-		papinv(post,invocs,"\t\t\tSystem.out.println(s);");
-		papinv(post,invocs,"\t}\n}");
+		papinv(lines,invocs,"import java.util.LinkedList;\n","lines");
+		papinv(lines,invocs,"public class SelfPrinter\n{","lines");
+		papinv(lines,invocs,"\tpublic static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s, String list)\n\t{","lines");
+		papinv(lines,invocs,"\t\tlines.add(s);","lines");
+		papinv(lines,invocs,"\t\ts = s.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\n\", \"\\\\n\").replace(\"\\\"\",\"\\\\\\\"\").replace(\"\\t\", \"\\\\t\");","lines");
+		papinv(lines,invocs,"\t\tString temp = \"\\t\\tpapinv(\" + list + \",invocs,\\\"\" + s + \"\\\",\\\"\" + list + \"\\\");\";","lines");
+		papinv(lines,invocs,"\t\tinvoc.add(temp);","lines");
+		papinv(lines,invocs,"\t}\n","lines");
+		papinv(lines,invocs,"\tpublic static void main(String[] args)\n\t{","lines");
+		papinv(lines,invocs,"\t\tLinkedList<String> lines = new LinkedList<String>();","lines");
+		papinv(lines,invocs,"\t\tLinkedList<String> invocs = new LinkedList<String>();","lines");
+		papinv(lines,invocs,"\t\tLinkedList<String> post = new LinkedList<String>();","lines");
+		papinv(post,invocs,"\t\tfor(String s : lines)","post");
+		papinv(post,invocs,"\t\t\tSystem.out.println(s);","post");
+		papinv(post,invocs,"\t\tfor(String s : invocs)","post");
+		papinv(post,invocs,"\t\t\tSystem.out.println(s);","post");
+		papinv(post,invocs,"\t\tfor(String s : post)","post");
+		papinv(post,invocs,"\t\t\tSystem.out.println(s);","post");
+		papinv(post,invocs,"\t}\n}","post");
 		for(String s : lines)
 			System.out.println(s);
 		for(String s : invocs)
