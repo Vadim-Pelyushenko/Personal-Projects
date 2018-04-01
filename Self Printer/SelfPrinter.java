6334d473d9304d4c6d807c3bd5ae8df2e9d518d8
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-31 17:21:07 -0700

    A fair deal done

--- Self Printer/SelfPrinter.java
+++ Self Printer/SelfPrinter.java
@@ -2,14 +2,26 @@ import java.util.LinkedList;
 
 public class SelfPrinter
 {
-	public static void printAndPrintCall(LinkedList<String> lines, LinkedList<String> invoc, String s) 
+	public static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s) 
 	{
 		lines.add(s);
-		invoc.add("printAndPrintCall(lines,invoc," + s + ");");
+		String temp = "papinv(lines,invocs,\"" + s + "\");";
+		invoc.add(temp.replace("\n", "\\n").replace("\\", "\\\\").replace("\"","\\\""));
 	} 
 	
 	public static void main(String[] args) 
 	{
-		
+		LinkedList<String> lines = new LinkedList<String>();
+		LinkedList<String> invocs = new LinkedList<String>();
+		papinv(lines,invocs,"import java.util.LinkedList\n");
+		papinv(lines,invocs,"public class SelfPrinter\n{\n");
+		papinv(lines,invocs,"public static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s)\n{");
+		papinv(lines,invocs,"lines.add(s);");
+		papinv(lines,invocs,"String temp = \"papinv(lines,invocs,\\\"\" + s + \"\\\");\";");
+		papinv(lines,invocs,"invoc.add(temp.replace(\"\\n\", \"\\\\n\").replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\\"\",\"\\\\\\\"\");");
+		for(String s : lines)
+			System.out.println(s);
+		for(String s : invocs)
+			System.out.println(s);
 	}
 }
