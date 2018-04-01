29084da3e4ff5f80991be9af60b87f8ac2a3eddd
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-31 18:19:23 -0700

    Prints out everything
    Now I need to evaluate it to check everything matches up

--- Self Printer/SelfPrinter.java
+++ Self Printer/SelfPrinter.java
@@ -16,7 +16,7 @@ public class SelfPrinter
 		LinkedList<String> invocs = new LinkedList<String>();
 		LinkedList<String> post = new LinkedList<String>();
 		papinv(lines,invocs,"import java.util.LinkedList\n");
-		papinv(lines,invocs,"public class SelfPrinter\n{\n");
+		papinv(lines,invocs,"public class SelfPrinter\n{");
 		papinv(lines,invocs,"\tpublic static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s)\n\t{");
 		papinv(lines,invocs,"\t\tlines.add(s);");
 		papinv(lines,invocs,"\t\ts = s.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\n\",\"\\\\n\").replace(\"\\\"\",\"\\\\\\\"\").replace(\"\\t\", \"\\\\t\");");
@@ -27,6 +27,13 @@ public class SelfPrinter
 		papinv(lines,invocs,"\t\tLinkedList<String> lines = new LinkedList<String>();");
 		papinv(lines,invocs,"\t\tLinkedList<String> invocs = new LinkedList<String>();");
 		papinv(lines,invocs,"\t\tLinkedList<String> post = new LinkedList<String>();");
+		papinv(post,invocs,"\t\tfor(String s : lines)");
+		papinv(post,invocs,"\t\t\tSystem.out.println(s);");
+		papinv(post,invocs,"\t\tfor(String s : invocs)");
+		papinv(post,invocs,"\t\t\tSystem.out.println(s);");
+		papinv(post,invocs,"\t\tfor(String s : post)");
+		papinv(post,invocs,"\t\t\tSystem.out.println(s);");
+		papinv(post,invocs,"\t}\n}");
 		for(String s : lines)
 			System.out.println(s);
 		for(String s : invocs)
