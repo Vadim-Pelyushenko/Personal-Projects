a7ab31131f1066ff7e8fec551f432dc8e3e3c7a6
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-31 19:03:23 -0700

    No longer uses LinkedList
    I think one of the rules were that I'm not allowed to use outside classes, but I can define my own
    objects if I wanted to. So instead of using a LinkedList, I've changed it to only use Strings to hold the
    entire output

--- Self Printer/SelfPrinter.java
+++ Self Printer/SelfPrinter.java
@@ -1,44 +1,38 @@
-import java.util.LinkedList;
-
 public class SelfPrinter
 {
-	public static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s, String list)
+	public static String lines = "";
+	public static String invocs = "";
+	public static String post = "";
+
+	public static void papinv(String s, boolean isPost)
 	{
-		lines.add(s);
+		if(isPost){post += s + "\n";}
+		else{lines += s + "\n";}
 		s = s.replace("\\", "\\\\").replace("\n", "\\n").replace("\"","\\\"").replace("\t", "\\t");
-		String temp = "\t\tpapinv(" + list + ",invocs,\"" + s + "\",\"" + list + "\");";
-		invoc.add(temp);
+		String temp = "\t\tpapinv(\"" + s + "\"," + isPost + ");";
+		invocs += temp + "\n";
 	}
 
 	public static void main(String[] args)
 	{
-		LinkedList<String> lines = new LinkedList<String>();
-		LinkedList<String> invocs = new LinkedList<String>();
-		LinkedList<String> post = new LinkedList<String>();
-		papinv(lines,invocs,"import java.util.LinkedList;\n","lines");
-		papinv(lines,invocs,"public class SelfPrinter\n{","lines");
-		papinv(lines,invocs,"\tpublic static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s, String list)\n\t{","lines");
-		papinv(lines,invocs,"\t\tlines.add(s);","lines");
-		papinv(lines,invocs,"\t\ts = s.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\n\", \"\\\\n\").replace(\"\\\"\",\"\\\\\\\"\").replace(\"\\t\", \"\\\\t\");","lines");
-		papinv(lines,invocs,"\t\tString temp = \"\\t\\tpapinv(\" + list + \",invocs,\\\"\" + s + \"\\\",\\\"\" + list + \"\\\");\";","lines");
-		papinv(lines,invocs,"\t\tinvoc.add(temp);","lines");
-		papinv(lines,invocs,"\t}\n","lines");
-		papinv(lines,invocs,"\tpublic static void main(String[] args)\n\t{","lines");
-		papinv(lines,invocs,"\t\tLinkedList<String> lines = new LinkedList<String>();","lines");
-		papinv(lines,invocs,"\t\tLinkedList<String> invocs = new LinkedList<String>();","lines");
-		papinv(lines,invocs,"\t\tLinkedList<String> post = new LinkedList<String>();","lines");
-		papinv(post,invocs,"\t\tfor(String s : lines)","post");
-		papinv(post,invocs,"\t\t\tSystem.out.println(s);","post");
-		papinv(post,invocs,"\t\tfor(String s : invocs)","post");
-		papinv(post,invocs,"\t\t\tSystem.out.println(s);","post");
-		papinv(post,invocs,"\t\tfor(String s : post)","post");
-		papinv(post,invocs,"\t\t\tSystem.out.println(s);","post");
-		papinv(post,invocs,"\t}\n}","post");
-		for(String s : lines)
-			System.out.println(s);
-		for(String s : invocs)
-			System.out.println(s);
-		for(String s : post)
-			System.out.println(s);
+		papinv("public class SelfPrinter\n{",false);
+		papinv("\tpublic static String lines = \"\";",false);
+		papinv("\tpublic static String invocs = \"\";",false);
+		papinv("\tpublic static String post = \"\";\n",false);
+		papinv("\tpublic static void papinv(String s, boolean isPost)\n\t{",false);
+		papinv("\t\tif(isPost){post += s + \"\\n\";}",false);
+		papinv("\t\telse{lines += s + \"\\n\";}",false);
+		papinv("\t\ts = s.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\n\", \"\\\\n\").replace(\"\\\"\",\"\\\\\\\"\").replace(\"\\t\", \"\\\\t\");",false);
+		papinv("\t\tString temp = \"\\t\\tpapinv(\\\"\" + s + \"\\\",\" + isPost + \");\";",false);
+		papinv("\t\tinvocs += temp + \"\\n\";",false);
+		papinv("\t}\n",false);
+		papinv("\tpublic static void main(String[] args)\n\t{",false);
+		papinv("\t\tSystem.out.print(lines);",true);
+		papinv("\t\tSystem.out.print(invocs);",true);
+		papinv("\t\tSystem.out.print(post);",true);
+		papinv("\t}\n}",true);
+		System.out.print(lines);
+		System.out.print(invocs);
+		System.out.print(post);
 	}
 }
