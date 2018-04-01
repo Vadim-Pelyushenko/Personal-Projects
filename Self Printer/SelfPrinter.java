24b34691886f115828d8c89824468967bc8b2903
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-03-31 18:16:53 -0700

    Prints out everything before, and up to invocations
    Now I need to have it print out the stuff that comes after the invocations. Which I'll be doing
    using "post".

--- Self Printer/SelfPrinter.java
+++ Self Printer/SelfPrinter.java
@@ -5,23 +5,33 @@ public class SelfPrinter
 	public static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s) 
 	{
 		lines.add(s);
-		String temp = "papinv(lines,invocs,\"" + s + "\");";
-		invoc.add(temp.replace("\n", "\\n").replace("\\", "\\\\").replace("\"","\\\""));
+		s = s.replace("\\", "\\\\").replace("\n", "\\n").replace("\"","\\\"").replace("\t", "\\t");
+		String temp = "\t\tpapinv(lines,invocs,\"" + s + "\");";
+		invoc.add(temp);
 	} 
 	
 	public static void main(String[] args) 
 	{
 		LinkedList<String> lines = new LinkedList<String>();
 		LinkedList<String> invocs = new LinkedList<String>();
+		LinkedList<String> post = new LinkedList<String>();
 		papinv(lines,invocs,"import java.util.LinkedList\n");
 		papinv(lines,invocs,"public class SelfPrinter\n{\n");
-		papinv(lines,invocs,"public static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s)\n{");
-		papinv(lines,invocs,"lines.add(s);");
-		papinv(lines,invocs,"String temp = \"papinv(lines,invocs,\\\"\" + s + \"\\\");\";");
-		papinv(lines,invocs,"invoc.add(temp.replace(\"\\n\", \"\\\\n\").replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\\"\",\"\\\\\\\"\");");
+		papinv(lines,invocs,"\tpublic static void papinv(LinkedList<String> lines, LinkedList<String> invoc, String s)\n\t{");
+		papinv(lines,invocs,"\t\tlines.add(s);");
+		papinv(lines,invocs,"\t\ts = s.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\n\",\"\\\\n\").replace(\"\\\"\",\"\\\\\\\"\").replace(\"\\t\", \"\\\\t\");");
+		papinv(lines,invocs,"\t\tString temp = \"\\t\\tpapinv(lines,invocs,\\\"\" + s + \"\\\");\";");
+		papinv(lines,invocs,"\t\tinvoc.add(temp);");
+		papinv(lines,invocs,"\t}\n");
+		papinv(lines,invocs,"\tpublic static void main(String[] args)\n\t{");
+		papinv(lines,invocs,"\t\tLinkedList<String> lines = new LinkedList<String>();");
+		papinv(lines,invocs,"\t\tLinkedList<String> invocs = new LinkedList<String>();");
+		papinv(lines,invocs,"\t\tLinkedList<String> post = new LinkedList<String>();");
 		for(String s : lines)
 			System.out.println(s);
 		for(String s : invocs)
+			System.out.println(s);
+		for(String s : post)
 			System.out.println(s);
 	}
 }
